4a28c47b385f6aa6b2d90ae3c7bae913cd928ef9
jothishwar <jothishwarvvr@gmail.com>
2022-11-07 19:09:53 +0530

    added onclick events

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,33 +1,54 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
 function TicTacToe(){
 
-	const handleClick = () => {
-		console.count('yes');
+	const [turn,setTurn] = useState("1");
+	const [cells,setCells] = useState(Array(9).fill(""));
+
+
+	const handleClick = (num) => {
+
+		if(cells[num] !== ""){
+			alert("Invalid Move");
+			return;
+		}
+		let squares=[...cells]
+		if(turn==="1"){
+			squares[num]='X';
+			setTurn("2");
+		}else{
+			squares[num]='O';
+			setTurn("1");
+		}
+
+		setCells(squares);
+		console.log(squares);
 	}
 
-	const Cell = () => {
-		return <td onClick={handleClick}>*</td>
+	const Cell = ({num}) => {
+		return <td onClick={() => handleClick(num)}>{cells[num]}</td>
 	}
 
 	return(
 		<div className='container'>
+
+			<p>Player <span>{turn}</span></p>
 			<table>
 				<tbody>
 					<tr>
-						<Cell />
-						<Cell />
-						<Cell />
+						<Cell num={0}/>
+						<Cell num={1}/>
+						<Cell num={2}/>
 					</tr>
 					<tr>
-						<Cell />
-						<Cell />
-						<Cell />
+						<Cell num={3}/>
+						<Cell num={4}/>
+						<Cell num={5}/>
 					</tr>
 					<tr>
-						<Cell />
-						<Cell />
-						<Cell />
+						<Cell num={6}/>
+						<Cell num={7}/>
+						<Cell num={8}/>
 					</tr>
 				</tbody>
 			</table>
