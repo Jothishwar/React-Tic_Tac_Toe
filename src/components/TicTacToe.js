9fb50beb77e0dc098875fc27c8b55cb3c9c07279
jothishwar <jothishwarvvr@gmail.com>
2022-11-07 19:55:26 +0530

    added logic of game

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -4,7 +4,48 @@ function TicTacToe(){
 
 	const [turn,setTurn] = useState("1");
 	const [cells,setCells] = useState(Array(9).fill(""));
+	const [winner,setWinner] = useState();
 
+	const checkWinner = (squares) => {
+		let combos={
+			across : [
+				[0,1,2],
+				[3,4,5],
+				[6,7,8]
+			],
+			down : [
+				[0,3,6],
+				[1,4,7],
+				[2,5,8]
+			],
+			diagonal : [
+				[0,4,8],
+				[2,4,6]
+			]
+		}
+
+		for (let combo in combos){
+			combos[combo].forEach((pattern)=>{
+				if (squares[pattern[0]] === '' ||
+					squares[pattern[1]] === '' ||
+					squares[pattern[2]] === ''
+				){
+					//do nothing
+				}else if(
+					squares[pattern[0]] === squares[pattern[1]] &&
+					squares[pattern[1]] === squares[pattern[2]]
+				){
+					setWinner(() => {
+						if(squares[pattern[0]] === 'X'){
+							return 1;
+						}
+						return 2;
+					});
+				}
+
+			})
+		}
+	}
 
 	const handleClick = (num) => {
 
@@ -21,8 +62,9 @@ function TicTacToe(){
 			setTurn("1");
 		}
 
+		checkWinner(squares);
 		setCells(squares);
-		console.log(squares);
+		// console.log(squares);
 	}
 
 	const Cell = ({num}) => {
@@ -52,6 +94,12 @@ function TicTacToe(){
 					</tr>
 				</tbody>
 			</table>
+
+			{winner && (
+				<>
+					<p>Player {winner} is the Winner</p>
+				</>
+			)}
 		</div>
 	)
 }
